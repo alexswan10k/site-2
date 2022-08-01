a6900f6f6fa1b9d4df18197236a35dca7c876654
Alex Swan <1506553+alexswan10k@users.noreply.github.com>
2022-08-01 11:05:21 +0100

    publish

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // assetPrefix: './',
   reactStrictMode: true,
-  // exportPathMap: async function() {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/test": { page: "/test" },
-  //   } 
-  // },
+  exportPathMap: async function() {
+    return {
+      "/": { page: "/" },
+      "/about-us": { page: "/about-us"},
+      "/test": { page: "/test" },
+    } 
+  },
   experimental: {
     images: {
       unoptimized: true
