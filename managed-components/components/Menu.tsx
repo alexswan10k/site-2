a72041f1e391ae09ad70edfd6f384aba176d3915
Alex Swan <1506553+alexswan10k@users.noreply.github.com>
2022-08-01 11:03:59 +0100

    animate menu

--- managed-components/components/Menu.tsx
+++ managed-components/components/Menu.tsx
@@ -1,17 +1,38 @@
 import Link from "next/link";
 import { FC } from "react";
 import { css } from '@emotion/css'
+import { motion } from "framer-motion";
 
 const Menu: FC<{ items: { displayName: string, link: string }[] }> = ({ items }) => {
     return <div className={css`
         display: flex;
     `}>
-        {items.map(item => <div key={item.displayName} className={css`
+        {items.map((item, idx) => <div key={item.displayName} className={css`
                 padding: 1rem
             `}>
+            <motion.div
+                initial={{ 
+                    opacity: 0.6, 
+                    // scale: 0.6 
+                }}
+                // animate= {{
+                //     opacity: 1,
+                //     scale: 1
+                // }}
+                // transition= {{
+                //     duration: 1,
+                //     delay: idx * 0.3
+                // }}
+                whileHover={{
+                    scale: 1.2,
+                    opacity: 1,
+                    transition: { duration: 1 },
+                }}>
                 <Link href={item.link}>{item.displayName}</Link>
-            </div>
-            )}
+            </motion.div>
+
+        </div>
+        )}
     </div>
 }
 
